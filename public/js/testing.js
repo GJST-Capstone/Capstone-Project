f38d16435dab4b5e916be081fde15a34cbd099da
Stacy Fabian <stacy.fabian@gmail.com>
2014-12-03 22:13:18 -0800

    add notes for what to do next and start building loop

--- public/js/testing.js
+++ public/js/testing.js
@@ -9,17 +9,6 @@
 
 // what does URL look like when search terms are entered? How can we dynamically build URL w/ search terms.
 
-// var channelID = ['UCD0nBMLdq_KbIK9u-mzpNkA',
-// 				'UCxTO69CggJGFQpZz-kE2k7g',
-// 				'UCdsvmOV_qYJUPqlUxs72Jtw',
-// 				'UCgBTevPW8fsH4pQNrLufOsQ',
-// 				'UCiP6wD_tYlYLYh3agzbByWQ',
-// 				'UCuY1W4AwhhgkB6rsJBtltUA',
-// 				'UCnUlSOVlCmoyQ6e2YQAGZZA',
-// 				'UCIJwWYOfsCfz6PjxbONYXSg'];
-
-// var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='+ channelID + '&key=AIzaSyDWCByDYIy-ow0OcChMq9QtoDrbem-xFLA';
-
 // var fullVideoData = [];
 
 
@@ -34,27 +23,49 @@
 // 	console.log(fullVideoData);
 // })
 
-var results = {};
+// Dec 3: build a for loop to run through the url to add a search term in correct place and if more than one search term, add in correct format. (if statement)
+// then run that through all the channels (insert channel ID from array of approved IDs)
+// push data into results array (change results object to array)
+// look into video format (mobile read, etc...)
 
-// stuff from Greg.
+var results = [];
+
+// written by greg
     var firstURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCD0nBMLdq_KbIK9u-mzpNkA&key=AIzaSyDWCByDYIy-ow0OcChMq9QtoDrbem-xFLA';
+    var firstURLwSearchTerms = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCIJwWYOfsCfz6PjxbONYXSg&q=abs&key={YOUR_API_KEY}'
+    // GREG SAYS: search terms (from drop down options) will be stored into an array.
+    // .... So to add them to the URL, I'll need to first pass that array to a string and then add to the URL call
+
     //.get callback for youtube data
     function vidDataCallback(data) {
       
-      results = data;
-      results.items.forEach(function (video){
-      	console.log(data);
-        video.id2 = video.id
-        delete video.id
+      results.push(data.items);
+      results.forEach(function (video){
+        video.id2 = video.id;
+        delete video.id;
       }); 
       //add the data to the collection
       //render
+      console.log(results);
       return results;
     }
     //get the data
     $.get(firstURL, null, vidDataCallback);
 
+// loop to build URLs
 
+var channelID = ['UCD0nBMLdq_KbIK9u-mzpNkA',
+       'UCxTO69CggJGFQpZz-kE2k7g',
+       'UCdsvmOV_qYJUPqlUxs72Jtw',
+       'UCgBTevPW8fsH4pQNrLufOsQ',
+       'UCiP6wD_tYlYLYh3agzbByWQ',
+       'UCuY1W4AwhhgkB6rsJBtltUA',
+       'UCnUlSOVlCmoyQ6e2YQAGZZA',
+       'UCIJwWYOfsCfz6PjxbONYXSg'];
+
+for (i=o; i<channelID.index; ++i) {
+  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='+ channelID[i] + '&key=AIzaSyDWCByDYIy-ow0OcChMq9QtoDrbem-xFLA';
+}
 
 // results should be an array of objects
 //		Objects should be the data.items[0] of each channel request.
